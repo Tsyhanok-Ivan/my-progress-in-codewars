95a207d035cd5e43d195de9afc710a40dcd529e3
Ivan <135434946+Tsyhanok-Ivan@users.noreply.github.com>
2023-10-17 13:48:39 +0300

    Create main.py

---
+++ Vowel Count/Disemvowel Trolls/main.py
@@ -0,0 +1,8 @@
+# Python 3.11
+# https://www.codewars.com/kata/52fba66badcd10859f00097e
+
+def disemvowel(string_):
+    for i in list("aeiouAEIOU"):
+        string_ = string_.replace(i, "")
+    return string_
+  
